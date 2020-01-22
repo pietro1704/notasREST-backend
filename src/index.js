2c421f44eaa2fd61c00f25686f5e2da029a2dfbd
Pietro Pugliesi <pietropugliesi@icloud.com>
2020-01-22 20:48:33 -0300

    start command node index.js, sem nodemon, pra heroku

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const routes = require('./routes')
 
 const init = async () => {
     const server = Hapi.server({
-        port: 3000,
+        port: process.env.PORT || 3000,
         host: 'localhost'
     })
 
